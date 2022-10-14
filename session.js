bf3e563cee2cd0bf22673218f83be361870f0010
gomunoooo <115713805+gomunoooo@users.noreply.github.com>
2022-10-14 19:44:24 +0700

    Create session.js

---
+++ session.js
@@ -0,0 +1,10 @@
+{
+  "name": "AganZ^OFC",
+  "version": "1.0.0",
+  "main": "index.js",
+  "license": "MIT",
+  "dependencies": {
+    "@adiwajshing/zifabotz": "^4.0.1",
+    "qrcode-terminal": "^0.12.0"
+  }
+}
